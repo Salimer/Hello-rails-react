a0101d053d716a2dda7ebdf69e2d6f3dc891806e
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-18 16:29:18 +0300

    Delete greetings_controller.rb

--- app/controllers/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class GreetingsController < ApplicationController
-    def random
-        random_greeting = Message.order('RANDOM()').first
-        render json: { greeting: random_greeting.content }
-    end
-end
